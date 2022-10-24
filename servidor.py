f69a3e4a5fc7c5f49069986c86743221b2b8bee8
Marco <A01746163@itesm.mx>
2022-10-24 11:27:46 -0500

    codigoServidor

---
+++ servidor.py
@@ -0,0 +1,15 @@
+from flask import Flask, request,jsonify,render_template
+
+#Generar el servidor (Back-end)
+
+servidorWeb = Flask(__name__)
+
+#Anotacion
+
+@servidorWeb.route("/test",methods=['GET'])
+def formulario():
+    return render_template('pagina.html')
+
+
+if __name__ == '__main__':
+    servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
