8bac354db27bf6aa0b6a1efa47cfce481094c8aa
Marco <A01746163@itesm.mx>
2022-10-25 09:59:47 -0500

    Servicios

--- servidor.py
+++ servidor.py
@@ -1,4 +1,8 @@
 from flask import Flask, request,jsonify,render_template
+from werkzug.utils import secure_filename
+from joblib import load
+import numpy as np
+import os
 
 #Generar el servidor (Back-end)
 
@@ -9,6 +13,26 @@ servidorWeb = Flask(__name__)
 @servidorWeb.route("/test",methods=['GET'])
 def formulario():
     return render_template('pagina.html')
+
+#Procesar Datos a travez del form
+@servidorWeb.route("/modeloIA",methods=['POST'])
+def modeloForm():
+    #Procesar datos de entrada
+    contenido = request.form
+    print(contenido)
+    return jsonify({"Resultado":"datos recibidos"})
+
+@servidorWeb.route("/modeloFile",methods=['POST'])
+def modeloFile():
+    f = request.files['file']
+    filename = secure_filename(f.filename)
+    path = os.path.join(os.getcwd,'files',filename)
+    f.save(path)
+    file = open(path,'r')
+    for line in file:
+        print(line)
+    
+    return jsonify({"Resultado":"datos recibidos"})
 
 
 if __name__ == '__main__':
